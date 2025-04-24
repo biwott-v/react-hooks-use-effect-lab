c66f5379f96f448b4b04f20629c142b75b03cc77
biwott-v <victor.biwott@student.moringaschool.com>
2025-04-24 08:26:15 +0300

    completes assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    let timeoutId;
+
+    function tick() {
+      setTimeRemaining((prevTime) => {
+        if (prevTime <= 1) {
+          onAnswered(false);
+          return 0;
+        } else {
+          timeoutId = setTimeout(tick, 1000);
+          return prevTime - 1;
+        }
+      });
+    }
+
+    timeoutId = setTimeout(tick, 1000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
